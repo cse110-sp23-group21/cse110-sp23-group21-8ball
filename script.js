9196ec1b80012b33755f82af9f1c9f561caf6b50
maarongross <97714828+maarongross@users.noreply.github.com>
2023-04-23 14:12:28 -0700

    Added ChatGPT changes for frontend

--- script.js
+++ script.js
@@ -1,7 +1,27 @@
-function ask_8ball(){
-  event.preventDefault(); // I do not know what this may do
-  document.getElementById("question").value = "";
-  var messages = ["yes", "no", "maybe", "ask again", "In the near future, all will be revealed"];
-  var randomMessage = messages[Math.floor(Math.random()*messages.length)];
+function ask_8ball() {
+  event.preventDefault();
+  var questionInput = document.getElementById("question");
+  var question = questionInput.value;
+  if (question.trim() === "") {
+    alert("Please enter a question!");
+    return;
+  }
+
+  // animate 8-ball
+  var eightBallImg = document.querySelector("img");
+  eightBallImg.classList.add("shake");
+  setTimeout(() => {
+    eightBallImg.classList.remove("shake");
+  }, 300);
+
+  questionInput.value = "";
+  var messages = [
+    "yes",
+    "no",
+    "maybe",
+    "ask again",
+    "In the near future, all will be revealed"
+  ];
+  var randomMessage = messages[Math.floor(Math.random() * messages.length)];
   document.getElementById("output").innerHTML = randomMessage;
 }
